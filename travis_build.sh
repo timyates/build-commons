c7a7ea24878d867af06db516f95612a03fa634f1
Danilo Pianini <danilo.pianini@unibo.it>
2017-04-11 19:26:38 +0200

    Update the build system

---
+++ travis_build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+openssl aes-256-cbc -K $encrypted_71fd7511358b_key -iv $encrypted_71fd7511358b_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
+bash prepare_environment.sh
+set -e
+./gradlew
+./gradlew publishPlugins --continue || exit 0
+./gradlew uploadArchives
